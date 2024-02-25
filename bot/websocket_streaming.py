1487c0f5af5fa74a41bbe2e0f0e01b3d202ee3fd
Gor B <92037197+Frogleim@users.noreply.github.com>
2024-02-25 23:54:26 +0400

    new streamers added

--- bot/websocket_streaming.py
+++ bot/websocket_streaming.py
@@ -1,9 +1,61 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import websocket
 import threading
 import json
 import psycopg2
+
+
+class DataBase:
+    def __init__(self):
+        self.user="postgres"
+        self.password="0000"
+        self.host="localhost"
+        self.port=5432
+        self.database="StreamSignal"
+
+    def connect(self):
+        return psycopg2.connect(
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            database=self.database
+        )
+
+    def insert_trades(self, quantity, price, side):
+        conn = self.connect()
+        cursor = conn.cursor()
+        if side:
+            cursor.execute("INSERT INTO trade_buyers (quantity, side, price) VALUES (%s, %s, %s)",
+                           (quantity, side, price))
+        else:
+            cursor.execute("INSERT INTO trade_sellers (quantity, side, price) VALUES (%s, %s, %s)",
+                           (quantity, side, price))
+        conn.commit()
+
+    def calculate_aggression_buyers(self):
+        conn = self.connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT SUM(quantity) AS total_quantity, COUNT(*) AS total_count FROM trade_buyers;")
+        record = cursor.fetchone()
+        print("Total Buyers Quantity:", record[0])
+        print("Total Buyers Count:", record[1])
+
+    def calculate_aggression_sellers(self):
+        conn = self.connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT SUM(quantity) AS total_quantity, COUNT(*) AS total_count FROM trade_sellers;")
+        record = cursor.fetchone()
+        print("Total Sellers Quantity:", record[0])
+        print("Total Sellers Count:", record[1])
+
+    def delete_rows_by_time(self, table_name, timestamp_column, start_time, end_time):
+        conn = self.connect()
+        cursor = conn.cursor()
+        delete_query = f"DELETE FROM {table_name} WHERE {timestamp_column} >= %s AND {timestamp_column} < %s;"
+        cursor.execute(delete_query, (start_time, end_time))
+        conn.commit()
 
 class Streaming(websocket.WebSocketApp):
     def __init__(self, url):
@@ -18,21 +70,29 @@ class Streaming(websocket.WebSocketApp):
         print('Websocket was opened')
 
     def message(self, msg):
-        print(msg)
+        data = json.loads(msg)
+        db = DataBase()
+        current_time = datetime.now()
+        start_time = current_time.replace(minute=(current_time.minute // 15) * 15, second=0, microsecond=0)
+        end_time = start_time + timedelta(minutes=15)
+        db.delete_rows_by_time('trade_buyers', 'timestamp_column', start_time, end_time)
+        db.delete_rows_by_time('trade_sellers', 'timestamp_column', start_time, end_time)
+        db.insert_trades(quantity=data['q'], price=data['p'], side=data['m'])
+        db.calculate_aggression_buyers()
+        db.calculate_aggression_sellers()
 
 
 
 
 
-class StreamingOrderBook(websocket.WebSocketApp):
-    def __init__(self, url, db_connection):
+class StreamingDepthBook(websocket.WebSocketApp):
+    def __init__(self, url):
         super().__init__(url=url, on_open=self.on_open)
         self.on_message = lambda ws, msg: self.message(msg)
         self.on_error = lambda ws, e: print('Error', e)
         self.on_close = lambda ws: print('Closing')
-        self.connect = db_connection
-        self.prices = []
-        self.volumes = []
+        self.total_bids = 0
+        self.total_asks = 0
         self.run_forever()
 
     def on_open(self, ws):
@@ -40,54 +100,50 @@ class StreamingOrderBook(websocket.WebSocketApp):
 
     def message(self, msg):
         data = json.loads(msg)
-        symbol = data.get('s')
-        if symbol == 'XRPUSDT':
-            price = float(data.get('b'))
-            volume = float(data.get('B'))
-            timestamp = datetime.now()
-            signal = self.analyze_trend(volume)
-            self.prices.append(price)
-            self.volumes.append(volume)
-            if signal != "No significant trend confirmation":
-                self.insert_signal(price, volume, timestamp, signal)
+        if 'b' in data and 'a' in data:
+            bids = data['b']
+            asks = data['a']
+            # Calculate total bids quantity
+            total_bids_quantity = sum(float(bid[1]) for bid in bids)
+            # Calculate total asks quantity
+            total_asks_quantity = sum(float(ask[1]) for ask in asks)
+            # Compare total bids and asks
+            if total_bids_quantity > total_asks_quantity:
+                print("There's more buying pressure (bullish)")
+            elif total_bids_quantity < total_asks_quantity:
+                print("There's more selling pressure (bearish)")
+            else:
+                print("Bids and asks are balanced")
 
 
 
 
-    def analyze_trend(self, volume):
-        if len(self.prices) < 2 or len(self.volumes) < 2:
-            print('No signal')
-            return "No significant trend confirmation"
+class ForcedOrders(websocket.WebSocketApp):
+    def __init__(self, url):
+        super().__init__(url=url, on_open=self.on_open)
+        self.on_message = lambda ws, msg: self.message(msg)
+        self.on_error = lambda ws, e: print('Error', e)
+        self.on_close = lambda ws: print('Closing')
+        self.forced_orders = []
+        self.run_forever()
 
-        current_price = self.prices[-1]
-        previous_price = self.prices[-2]
-        current_volume = self.volumes[-1]
-        previous_volume = self.volumes[-2]
+    def on_open(self, ws):
+        print('Websocket was opened')
 
-        if current_price > previous_price and current_volume > previous_volume:
-            print('Potential buy signal')
-            if volume > current_volume - previous_volume:
-                return "Buy signal: Rising prices with increasing volume"
-        elif current_price > previous_price and current_volume < previous_volume:
-            if volume < current_volume - previous_volume:
-                print('Potential buy signal')
-                return "Sell signal: Rising prices with decreasing volume"
-        return 'No significant trend confirmation'
+    def message(self, msg):
+        data = json.loads(msg)
+        if data.get('e') == 'forceOrder':
+            self.forced_orders.append(data)
+            self.algorithmic_trading()
 
-    def insert_signal(self, price, volume, timestamp, signal):
-        try:
-            cursor =  self.connect.cursor()
-            cursor.execute("INSERT INTO order_book_data (price, volume, timestamp) VALUES (%s, %s, %s)", (price, volume, timestamp))
-            self.connect.commit()
-            cursor.execute("INSERT INTO trend_signals (timestamp, signal, volumes) VALUES (%s, %s, %s)", (timestamp, signal, volume))
+    def algorithmic_trading(self):
+        # Your algorithmic trading logic here
+        # Example: Adjust trading parameters based on forced liquidation events
+        frequency = len(self.forced_orders)
+        severity = sum(float(order['o']['q']) for order in self.forced_orders)
 
-            self.connect.commit()
-
-            cursor.close()
-        except (Exception, psycopg2.Error) as error:
-            print("Error while inserting data to PostgreSQL:", error)
-
-
+        print(f"Frequency of forced liquidations: {frequency}")
+        print(f"Total severity of forced liquidations: {severity}")
 
 
 if __name__ == '__main__':
@@ -102,4 +158,7 @@ if __name__ == '__main__':
     #     print("Connected to PostgreSQL")
     # except (Exception, psycopg2.Error) as error:
     #     print("Error while connecting to PostgreSQL:", error)
-    threading.Thread(target=Streaming, args=('wss://fstream.binance.com/ws/xrpusdt@trade', )).start()
+    threading.Thread(target=Streaming, args=('wss://fstream.binance.com/ws/xrpusdt@aggTrade', )).start()
+    # threading.Thread(target=StreamingDepthBook, args=('wss://fstream.binance.com/ws/xrpusdt@depth', )).start()
+    # threading.Thread(target=ForcedOrders, args=('wss://fstream.binance.com/ws/xrpusdt@forceOrder', )).start()
+
